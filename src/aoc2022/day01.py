9abc4559f178acc495351aa1a2cd403f2f08234e
Ken Smith <ken@klsmith.me>
2023-08-12 23:10:23 +0000

    Clean up 2022 day01

--- src/aoc2022/day01.py
+++ src/aoc2022/day01.py
@@ -1,8 +1,7 @@
 """ Solution to https://adventofcode.com/2022/day/1"""
-
-from utils.core import split_at_blanklines, parse_puzzle_input
+from utils.core import split_at_blanklines
 from pipe import Pipe, map, sort, take
-import sys
+
 
 # Input parsing
 
@@ -19,24 +18,32 @@ def parse(input):
 
 @Pipe
 def sorted_totals(calories):
+    """
+    Return a collection of all the `calorie` totals, sorted in descending order
+    """
     return calories | map(sum) | sort(reverse=True)
 
 
 def top_n_capacity_sum(n, calories):
+    """
+    Return the sum of the top `n` totals
+    """
     return sum(calories | sorted_totals | take(n))
 
 # Puzzle solutions
 
 
 def day01_part1(input):
+    """
+    Find the Elf carrying the most Calories. 
+    How many total Calories is that Elf carrying?
+    """
     return top_n_capacity_sum(1, input)
 
 
 def day01_part2(input):
+    """
+    Find the top three Elves carrying the most Calories. 
+    How many Calories are those Elves carrying in total?
+    """
     return top_n_capacity_sum(3, input)
-
-
-if __name__ == "__main__":
-    input = parse_puzzle_input(parse, year=2022, day=1, filename=sys.argv[1])
-    print(day01_part1(input))
-    print(day01_part2(input))
