076fcc4e815f6b5d503cb37b795f4ec6c411d4c6
Ken Smith <ken@klsmith.me>
2023-08-13 00:56:46 +0000

    Add skeleton solution map for 2022

---
+++ src/aoc2022/__init__.py
@@ -0,0 +1,30 @@
+from aoc2022.day01 import day01_part1, day01_part2
+from aoc2022.day02 import day02_part1, day02_part2
+
+solutions = {
+    1:  [day01_part1, day01_part2],
+    2:  [day02_part1, day02_part2],
+    # 3:  [day03_part1, day03_part2],
+    # 4:  [day04_part1, day04_part2],
+    # 5:  [day05_part1, day05_part2],
+    # 6:  [day06_part1, day06_part2],
+    # 7:  [day07_part1, day07_part2],
+    # 8:  [day08_part1, day08_part2],
+    # 9:  [day09_part1, day09_part2],
+    # 10: [day10_part1, day10_part2],
+    # 11: [day11_part1, day11_part2],
+    # 12: [day12_part1, day12_part2],
+    # 13: [day13_part1, day13_part2],
+    # 14: [day14_part1, day14_part2],
+    # 15: [day15_part1, day15_part2],
+    # 16: [day16_part1, day16_part2],
+    # 17: [day17_part1, day17_part2],
+    # 18: [day18_part1, day18_part2],
+    # 19: [day19_part1, day19_part2],
+    # 20: [day20_part1, day20_part2],
+    # 21: [day21_part1, day21_part2],
+    # 22: [day22_part1, day22_part2],
+    # 23: [day23_part1, day23_part2],
+    # 24: [day24_part1, day24_part2],
+    # 25: [day25_part1, day25_part2]
+}
