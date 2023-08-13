45eb2d12cb1ea041ee45a0f67df01c13ae7902a0
Ken Smith <ken@klsmith.me>
2023-08-13 17:55:00 +0000

    2022 day03 part1 soln

--- src/aoc2022/day03.py
+++ src/aoc2022/day03.py
@@ -1,5 +1,7 @@
 """Solution to https://adventofcode.com/2022/day/3"""
-# from utils.core import AoCSolution
+from more_itertools import sliced
+from pipe import map, Pipe
+from utils.core import AoCSolution
 
 # Input parsing
 
@@ -10,9 +12,33 @@ def parse(input):
 # Puzzle logic
 
 
+def split_in_half(s: str):
+    return sliced(s, len(s)//2)
+
+
+def overlap(splits):
+    s1, s2 = splits
+    return set(s1).intersection(set(s2)).pop()
+
+
+def overlaps(splits_seq):
+    return splits_seq | map(overlap)
+
+
+def priority(c: str):
+    if c.isupper():
+        return ord(c) - ord("A") + 27
+    else:
+        return ord(c) - ord("a") + 1
+
+
+def overlap_priority_sum(seq):
+    return sum(seq | map(split_in_half) | map(overlap) | map(priority))
+
 # Puzzle solutions
 
-# day03_soln = \
-#     AoCSolution(parse,
-#                 p1=
-#                 p2=)
+
+day03_soln = \
+    AoCSolution(parse,
+                p1=overlap_priority_sum,
+                p2=overlap_priority_sum)
--- tests/aoc2022/test_day03.py
+++ tests/aoc2022/test_day03.py
@@ -1,12 +1,46 @@
-# from aoc2022.day03 import day03_soln
+from aoc2022.day03 import split_in_half, overlap, priority, \
+    overlap_priority_sum, day03_soln
 from utils.core import standard_puzzle_input
+from pipe import map
+
+d03_sample = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""".splitlines()
+
+
+def test_split_in_half():
+    assert ["vJrwpWtwJgWr", "hcsFMMfFFhFp"] == \
+        list(split_in_half(d03_sample[0]))
+
+
+def test_overlap():
+    assert "p" == overlap(["vJrwpWtwJgWr", "hcsFMMfFFhFp"])
+
+
+def test_overlaps():
+    assert ["p", "L", "P", "v", "t", "s"] == \
+        list(d03_sample | map(split_in_half) | map(overlap))
+
+
+def test_priority():
+    assert 1 == priority("a")
+    assert 26 == priority("z")
+    assert 27 == priority("A")
+    assert 52 == priority("Z")
+
+
+def test_overlap_priority_sum():
+    assert 157 == overlap_priority_sum(d03_sample)
 
 
 d03_input = standard_puzzle_input(year=2022, day=3)
 
 
-# def test_day03_part1_soln():
-#     assert 1 == day03_soln.part1(d03_input)
+def test_day03_part1_soln():
+    assert 7597 == day03_soln.part1(d03_input)
 
 
 # def test_day03_part2_soln():
