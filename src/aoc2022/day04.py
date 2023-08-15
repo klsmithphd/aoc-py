1af31294274864447c235b43020e416dbb5fa364
Ken Smith <ken@klsmith.me>
2023-08-15 00:53:35 +0000

    2022 day04 part1 soln

--- src/aoc2022/day04.py
+++ src/aoc2022/day04.py
@@ -1,18 +1,40 @@
 """Solution to https://adventofcode.com/2022/day/4"""
-# from utils.core import AoCSolution
+from pipe import map, filter
+from utils.core import AoCSolution
 
 # Input parsing
 
 
+def parse_range(rnge):
+    l, r = rnge.split("-")
+    return [int(l), int(r)]
+
+
+def parse_line(line):
+    return [parse_range(x) for x in line.split(",")]
+
+
 def parse(input):
-    return input
+    return input | map(parse_line)
 
 # Puzzle logic
 
 
+def isfully_contained(i1, i2):
+    l1, r1 = i1
+    l2, r2 = i2
+    return (l2 <= l1 <= r1 <= r2) or \
+           (l1 <= l2 <= r2 <= r1)
+
+
+def fully_contained_count(input):
+    return sum(1 for x in input if isfully_contained(*x))
+    # return len(list(input | filter(lambda row: isfully_contained(*row))))
+
 # Puzzle solutions
 
-# day04_soln = \
-#     AoCSolution(parse,
-#                 p1=
-#                 p2=)
+
+day04_soln = \
+    AoCSolution(parse,
+                p1=fully_contained_count,
+                p2=None)
--- tests/aoc2022/test_day04.py
+++ tests/aoc2022/test_day04.py
@@ -1,12 +1,41 @@
-# from aoc2022.day04 import day04_soln
+from aoc2022.day04 import day04_soln, parse, isfully_contained, fully_contained_count
 from utils.core import standard_puzzle_input
+
+d04_sample = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""".splitlines()
+
+
+def test_parse():
+    assert [[[2, 4], [6, 8]],
+            [[2, 3], [4, 5]],
+            [[5, 7], [7, 9]],
+            [[2, 8], [3, 7]],
+            [[6, 6], [4, 6]],
+            [[2, 6], [4, 8]]] == list(parse(d04_sample))
+
+
+def test_isfully_contained():
+    assert False == isfully_contained([2, 4], [6, 8])
+    assert False == isfully_contained([2, 3], [4, 5])
+    assert False == isfully_contained([5, 7], [7, 9])
+    assert True == isfully_contained([2, 8], [3, 7])
+    assert True == isfully_contained([6, 6], [4, 6])
+    assert False == isfully_contained([2, 6], [4, 8])
+
+
+def test_fully_contained_count():
+    assert 2 == fully_contained_count(parse(d04_sample))
 
 
 d04_input = standard_puzzle_input(year=2022, day=4)
 
 
-# def test_day04_part1_soln():
-#     assert 1 == day04_soln.part1(d04_input)
+def test_day04_part1_soln():
+    assert 584 == day04_soln.part1(d04_input)
 
 
 # def test_day04_part2_soln():
