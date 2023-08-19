b484d32fca7283e40bb8724ae2dce04f2a143c26
Ken Smith <ken@klsmith.me>
2023-08-19 22:51:38 +0000

    progress on parsing

--- src/aoc2022/day05.py
+++ src/aoc2022/day05.py
@@ -1,14 +1,28 @@
 """Solution to https://adventofcode.com/2022/day/5"""
+from itertools import zip_longest
 from utils.core import AoCSolution, split_at_blanklines
+from pipe import Pipe, islice, map, reverse, skip, skip_while
 from parse import parse
 
 
 # Input parsing
 
+
+def parse_stack(stack):
+    crates = list(
+        reversed(list(stack | skip_while(lambda x: x == ' '))) | skip(1))
+    return crates
+
+
 def parse_stacks(stacks):
-    return {1: ["Z", "N"],
-            2: ["M", "C", "D"],
-            3: ["P"]}
+    # return {1: ["Z", "N"],
+    #         2: ["M", "C", "D"],
+    #         3: ["P"]}
+
+    crate_stacks = list(zip_longest(*stacks, fillvalue=' ') |
+                        islice(1, None, 4) |
+                        map(parse_stack))
+    return dict(zip(range(1, len(crate_stacks)+1), crate_stacks))
 
 
 def parse_move(move):
