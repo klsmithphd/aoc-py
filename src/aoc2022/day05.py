cd958ce85b821b6c97e16c0dde0798b01ea0d995
Ken Smith <ken@klsmith.me>
2023-08-19 22:13:37 +0000

    parsing moves

--- src/aoc2022/day05.py
+++ src/aoc2022/day05.py
@@ -1,11 +1,28 @@
 """Solution to https://adventofcode.com/2022/day/5"""
-# from utils.core import AoCSolution
+from utils.core import AoCSolution, split_at_blanklines
+from parse import parse
+
 
 # Input parsing
 
+def parse_stacks(stacks):
+    return {1: ["Z", "N"],
+            2: ["M", "C", "D"],
+            3: ["P"]}
 
-def parse(input):
-    return input
+
+def parse_move(move):
+    return parse("move {qty:d} from {from:d} to {to:d}", move).named
+
+
+def parse_moves(moves):
+    return [parse_move(move) for move in moves]
+
+
+def parse_input(input):
+    stacks, moves = split_at_blanklines(input)
+    return {"stacks": parse_stacks(stacks),
+            "moves": parse_moves(moves)}
 
 # Puzzle logic
 
--- tests/aoc2022/test_day05.py
+++ tests/aoc2022/test_day05.py
@@ -1,8 +1,29 @@
-# from aoc2022.day05 import day05_soln
+from aoc2022.day05 import parse_input
 from utils.core import standard_puzzle_input
 
+d05_s01_raw = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
-d05_input = standard_puzzle_input(year=2022, day=5)
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2""".splitlines()
+
+d05_s01 = parse_input(d05_s01_raw)
+
+
+def test_parse_input():
+    assert d05_s01 == {"stacks": {1: ["Z", "N"],
+                                  2: ["M", "C", "D"],
+                                  3: ["P"]},
+                       "moves": [{"qty": 1, "from": 2, "to": 1},
+                                 {"qty": 3, "from": 1, "to": 3},
+                                 {"qty": 2, "from": 2, "to": 1},
+                                 {"qty": 1, "from": 1, "to": 2}]}
+
+# d05_input = standard_puzzle_input(year=2022, day=5)
 
 
 # def test_day05_part1_soln():
