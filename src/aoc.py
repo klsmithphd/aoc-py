cc713a8987370022e5b8f8515e12238fbbdee633
Ken Smith <ken@klsmith.me>
2023-08-26 22:42:40 +0000

    Tweak output formatting of driver program

--- src/aoc.py
+++ src/aoc.py
@@ -11,13 +11,23 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("year", type=int, help="Puzzle year")
     parser.add_argument("day", type=int, help="Puzzle day")
-    parser.add_argument("--input", help="[Optional] puzzle input")
+    parser.add_argument("-i", "--input", help="[Optional] puzzle input")
+    parser.add_argument("-v", "--verbose", help="[Optional] verbose printing",
+                        action='store_true')
     args = parser.parse_args()
 
     filename = args.input if args.input else input_path(args.year, args.day)
     input = puzzle_input(filename)
 
     soln = solutions[args.year][args.day]
-    print(f"Advent of Code {args.year}, Day {args.day}, using {filename}")
-    print(f"Part 1 solution: {soln.part1(input)}")
-    print(f"Part 2 solution: {soln.part2(input)}")
+    if (args.verbose):
+        print(f"Advent of Code {args.year}, Day {args.day}, using {filename}")
+        print()
+        print("Part 1 solution:")
+        print(soln.part1_doc())
+    print(soln.part1(input))
+    if (args.verbose):
+        print()
+        print("Part 2 solution:")
+        print(soln.part2_doc())
+    print(soln.part2(input))
