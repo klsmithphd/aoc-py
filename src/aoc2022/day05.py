bcd005fb64410fafd2fc46ee5fa90ace1724f784
Ken Smith <ken@klsmith.me>
2023-08-26 20:35:22 +0000

    2022 Day05 part1 soln

--- src/aoc2022/day05.py
+++ src/aoc2022/day05.py
@@ -1,4 +1,5 @@
 """Solution to https://adventofcode.com/2022/day/5"""
+from functools import reduce
 from itertools import zip_longest
 from utils.core import AoCSolution, split_at_blanklines
 from pipe import Pipe, islice, map, reverse, skip, take_while
@@ -25,7 +26,7 @@ def parse_stacks(stacks):
 
 
 def parse_move(move):
-    return parse("move {qty:d} from {from:d} to {to:d}", move).named
+    return parse("move {:d} from {:d} to {:d}", move).fixed
 
 
 def parse_moves(moves):
@@ -39,8 +40,31 @@ def parse_input(input):
 
 # Puzzle logic
 
+# Imperative style
+# def move(stacks, move):
+#     qty, src, dst = move
+#     for i in range(qty):
+#         stacks[dst].append(stacks[src].pop())
+#     return stacks
+
+
+def step(stacks, move):
+    qty, src, dst = move
+    popped = list(reversed(stacks[src][-qty:]))
+    return {**stacks, src: stacks[src][:-qty], dst: stacks[dst]+popped}
+
+
+def stack_tops(stacks):
+    return ''.join(stacks.values() | map(lambda s: s[-1]))
+
 
 # Puzzle solutions
+
+def part1(input):
+    stacks = input['stacks']
+    moves = input['moves']
+    final_stacks = reduce(step, moves, stacks)
+    return stack_tops(final_stacks)
 
 # day05_soln = \
 #     AoCSolution(parse,
--- tests/aoc2022/test_day05.py
+++ tests/aoc2022/test_day05.py
@@ -1,4 +1,4 @@
-from aoc2022.day05 import parse_input
+from aoc2022.day05 import parse_input, part1, step, stack_tops
 from utils.core import standard_puzzle_input
 
 d05_s01_raw = """    [D]    
@@ -12,18 +12,47 @@ move 2 from 2 to 1
 move 1 from 1 to 2""".splitlines()
 
 d05_s01 = parse_input(d05_s01_raw)
+d05_input = parse_input(standard_puzzle_input(year=2022, day=5))
 
 
 def test_parse_input():
-    assert d05_s01 == {"stacks": {1: ["Z", "N"],
-                                  2: ["M", "C", "D"],
-                                  3: ["P"]},
-                       "moves": [{"qty": 1, "from": 2, "to": 1},
-                                 {"qty": 3, "from": 1, "to": 3},
-                                 {"qty": 2, "from": 2, "to": 1},
-                                 {"qty": 1, "from": 1, "to": 2}]}
+    assert d05_s01 == {"stacks": {1: ['Z', 'N'],
+                                  2: ['M', 'C', 'D'],
+                                  3: ['P']},
+                       "moves": [(1, 2, 1),
+                                 (3, 1, 3),
+                                 (2, 2, 1),
+                                 (1, 1, 2)]}
 
-# d05_input = standard_puzzle_input(year=2022, day=5)
+
+d05_s01_part1_step0 = d05_s01['stacks']
+d05_s01_part1_step1 = {1: ['Z', 'N', 'D'],
+                       2: ['M', 'C'],
+                       3: ['P']}
+d05_s01_part1_step2 = {1: [],
+                       2: ['M', 'C'],
+                       3: ['P', 'D', 'N', 'Z']}
+d05_s01_part1_step3 = {1: ['C', 'M'],
+                       2: [],
+                       3: ['P', 'D', 'N', 'Z']}
+d05_s01_part1_step4 = {1: ['C'],
+                       2: ['M'],
+                       3: ['P', 'D', 'N', 'Z']}
+
+
+def test_step():
+    assert d05_s01_part1_step1 == step(d05_s01_part1_step0, (1, 2, 1))
+    assert d05_s01_part1_step2 == step(d05_s01_part1_step1, (3, 1, 3))
+    assert d05_s01_part1_step3 == step(d05_s01_part1_step2, (2, 2, 1))
+    assert d05_s01_part1_step4 == step(d05_s01_part1_step3, (1, 1, 2))
+
+
+def test_stack_tops():
+    assert 'CMZ' == stack_tops(d05_s01_part1_step4)
+
+
+def test_day05_part1_soln():
+    assert 'CNSZFDVLJ' == part1(d05_input)
 
 
 # def test_day05_part1_soln():
