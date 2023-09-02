d9b460f313292c8e1b8dd5f3fc1075f0ebbb4542
Ken Smith <ken@klsmith.me>
2023-09-02 21:27:50 +0000

    2022 day07 parsing logic

--- src/aoc2022/day07.py
+++ src/aoc2022/day07.py
@@ -1,11 +1,46 @@
 """Solution to https://adventofcode.com/2022/day/7"""
+from functools import reduce
+from more_itertools import split_before
+from toolz import assoc_in, concatv
 # from utils.core import AoCSolution
 
 # Input parsing
 
 
+def change_dir(state, dir):
+    if (dir == '..'):
+        newpath = state['path'][:-1]
+        tree = state['tree']
+    else:
+        newpath = state['path'] + [dir]
+        tree = assoc_in(state['tree'], newpath, {})
+    return {'tree': tree, 'path': newpath}
+
+
+def process_item(item: str):
+    if (item.startswith("dir")):
+        return (item[4:], {})
+    else:
+        size, name = item.split()
+        return (name, int(size))
+
+
+def list_dir(state, contents):
+    return assoc_in(state, ['tree'] + state['path'], dict(process_item(x) for x in contents))
+
+
+def process_cmd(state, cmd):
+    if (cmd[0].startswith("$ cd")):
+        return change_dir(state, cmd[0][5:])
+    else:
+        return list_dir(state, cmd[1:])
+
+
 def parse(input):
-    return input
+    init_state = {'tree': {}, 'path': []}
+    cmds = split_before(input, lambda s: s.startswith('$'))
+    return reduce(process_cmd, cmds, init_state)['tree']
+
 
 # Puzzle logic
 
--- tests/aoc2022/test_day07.py
+++ tests/aoc2022/test_day07.py
@@ -1,8 +1,51 @@
-# from aoc2022.day07 import day07_soln
+from aoc2022.day07 import parse
 from utils.core import standard_puzzle_input
 
+d07_s01_raw = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k""".splitlines()
 
-d03_input = standard_puzzle_input(year=2022, day=7)
+d07_s01 = {"/":
+           {"a":
+            {"e":
+             {"i": 584},
+                "f": 29116,
+                "g": 2557,
+                "h.lst": 62596},
+               "b.txt": 14848514,
+               "c.dat": 8504156,
+               "d":
+               {"j": 4060174,
+                "d.log": 8033020,
+                "d.ext": 5626152,
+                "k": 7214296}}}
+
+
+def test_parse():
+    assert d07_s01 == parse(d07_s01_raw)
+
+
+# d03_input = standard_puzzle_input(year=2022, day=7)
 
 
 # def test_day07_part1_soln():
