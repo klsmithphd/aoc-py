3ca1802f911a4eabe61e0a904c56c1de1bf37eaf
Ken Smith <ken@klsmith.me>
2023-09-04 21:35:27 +0000

    2022 Day 08 Part 1

--- src/aoc2022/day08.py
+++ src/aoc2022/day08.py
@@ -1,16 +1,55 @@
 """Solution to https://adventofcode.com/2022/day/8"""
 # from utils.core import AoCSolution
+from more_itertools import flatten
+
 
 # Input parsing
 
 
 def parse(input):
-    return input
+    return [[int(x) for x in line] for line in input]
 
 # Puzzle logic
 
 
+def transpose(m):
+    return [list(col) for col in zip(*m)]
+
+
+def greatest_so_far(row):
+    mx = -1
+    for x in row:
+        if x > mx:
+            mx = x
+            yield 1
+        else:
+            yield 0
+
+
+def trees_visible_from_left(row):
+    return list(greatest_so_far(row))
+
+
+def trees_visible_in_row(row):
+    return list(map(lambda a, b: a or b,
+                    trees_visible_from_left(row),
+                    reversed(trees_visible_from_left(reversed(row)))))
+
+
+def trees_visible_horiz(grid):
+    return [trees_visible_in_row(row) for row in grid]
+
+
+def trees_visible_count(grid):
+    return sum(map(lambda a, b: a or b,
+                   flatten(trees_visible_horiz(grid)),
+                   flatten(transpose(trees_visible_horiz(transpose(grid))))))
+
+
 # Puzzle solutions
+
+def part1(input):
+    return trees_visible_count(input)
 
 # day08_soln = \
 #     AoCSolution(parse,
--- tests/aoc2022/test_day08.py
+++ tests/aoc2022/test_day08.py
@@ -1,12 +1,49 @@
-# from aoc2022.day08 import day08_soln
+from aoc2022.day08 import parse, part1, \
+    trees_visible_from_left, trees_visible_in_row, trees_visible_count
 from utils.core import standard_puzzle_input
 
+d08_s01_raw = """30373
+25512
+65332
+33549
+35390""".splitlines()
 
-d03_input = standard_puzzle_input(year=2022, day=8)
+d08_s01 = [[3, 0, 3, 7, 3],
+           [2, 5, 5, 1, 2],
+           [6, 5, 3, 3, 2],
+           [3, 3, 5, 4, 9],
+           [3, 5, 3, 9, 0]]
 
 
-# def test_day08_part1_soln():
-#     assert 1 == day08_soln.part1(d08_input)
+def test_parse():
+    assert d08_s01 == parse(d08_s01_raw)
+
+
+def test_trees_visible_from_left():
+    assert [1, 0, 0, 1, 0] == trees_visible_from_left(d08_s01[0])
+    assert [1, 1, 0, 0, 0] == trees_visible_from_left(d08_s01[1])
+    assert [1, 0, 0, 0, 0] == trees_visible_from_left(d08_s01[2])
+    assert [1, 0, 1, 0, 1] == trees_visible_from_left(d08_s01[3])
+    assert [1, 1, 0, 1, 0] == trees_visible_from_left(d08_s01[4])
+
+
+def test_trees_visible_in_row():
+    assert [1, 0, 0, 1, 1] == trees_visible_in_row(d08_s01[0])
+    assert [1, 1, 1, 0, 1] == trees_visible_in_row(d08_s01[1])
+    assert [1, 1, 0, 1, 1] == trees_visible_in_row(d08_s01[2])
+    assert [1, 0, 1, 0, 1] == trees_visible_in_row(d08_s01[3])
+    assert [1, 1, 0, 1, 1] == trees_visible_in_row(d08_s01[4])
+
+
+def test_trees_visible_count():
+    assert 21 == trees_visible_count(d08_s01)
+
+
+d08_input = parse(standard_puzzle_input(year=2022, day=8))
+
+
+def test_day08_part1_soln():
+    assert 1538 == part1(d08_input)
 
 
 # def test_day08_part2_soln():
