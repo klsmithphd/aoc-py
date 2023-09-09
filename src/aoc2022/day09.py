3ca160f68364a6dd37ea882b463708ee98e57fe7
Ken Smith <ken@klsmith.me>
2023-09-09 22:34:40 +0000

    2022 day09 part1

--- src/aoc2022/day09.py
+++ src/aoc2022/day09.py
@@ -1,16 +1,84 @@
 """Solution to https://adventofcode.com/2022/day/9"""
+from itertools import accumulate, repeat
+from more_itertools import flatten
+from operator import add, sub
 # from utils.core import AoCSolution
+
+# Constants
+
+MOVES = {
+    'R': (1, 0),
+    'L': (-1, 0),
+    'U': (0, 1),
+    'D': (0, -1)
+}
+
 
 # Input parsing
 
 
+def parse_line(line):
+    char, count = line.split()
+    return (char, int(count))
+
+
 def parse(input):
-    return input
+    return flatten(repeat(*parse_line(line)) for line in input)
 
 # Puzzle logic
 
+# Move to a utility module
+
+
+def tmap(f, *args):
+    return tuple(map(f, *args))
+
+
+def cap(num):
+    return num // 2 if num % 2 == 0 else num
+
+
+def tail_delta(tail, head):
+    """
+    Compute the amount by which the tail position should change based on the
+    new position of the knot in front of it
+    """
+    diff = tmap(sub, head, tail)
+    if (abs(diff[0]) <= 1 and abs(diff[1]) <= 1):
+        return (0, 0)
+    else:
+        return tmap(cap, diff)
+
+
+def move_tail(tail, head):
+    """
+    Update the tail position based on the knot in front of it
+    """
+    return tmap(add, tail, tail_delta(tail, head))
+
+
+def step(chain, cmd):
+    tail, head = chain
+    newhead = tmap(add, head, MOVES[cmd])
+    return [move_tail(tail, newhead), newhead]
+
+
+def all_moves(cmds):
+    return accumulate(cmds, step, initial=[(0, 0), (0, 0)])
+
+
+def unique_tail_positions(cmds):
+    return set(x[0] for x in all_moves(cmds))
+
+
+def unique_tail_pos_count(cmds):
+    return len(unique_tail_positions(cmds))
+
 
 # Puzzle solutions
+
+def part1(input):
+    return unique_tail_pos_count(input)
 
 # day09_soln = \
 #     AoCSolution(parse,
--- tests/aoc2022/test_day09.py
+++ tests/aoc2022/test_day09.py
@@ -1,12 +1,77 @@
-# from aoc2022.day09 import day09_soln
+from aoc2022.day09 import parse, part1, \
+    step, all_moves, unique_tail_positions, unique_tail_pos_count
+from toolz import last
 from utils.core import standard_puzzle_input
 
+d09_s01_raw = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2""".splitlines()
 
-d09_input = standard_puzzle_input(year=2022, day=9)
+d09_s01 = ['R', 'R', 'R', 'R',
+           'U', 'U', 'U', 'U',
+           'L', 'L', 'L',
+           'D',
+           'R', 'R', 'R', 'R',
+           'D',
+           'L', 'L', 'L', 'L', 'L',
+           'R', 'R']
 
 
-# def test_day09_part1_soln():
-#     assert 1 == day09_soln.part1(d09_input)
+def test_parse():
+    assert d09_s01 == list(parse(d09_s01_raw))
+
+
+def test_step():
+    assert [(0, 0), (1, 0)] == step([(0, 0), (0, 0)], 'R')
+    assert [(1, 0), (2, 0)] == step([(0, 0), (1, 0)], 'R')
+    assert [(2, 0), (3, 0)] == step([(1, 0), (2, 0)], 'R')
+    assert [(3, 0), (4, 0)] == step([(2, 0), (3, 0)], 'R')
+    assert [(3, 0), (4, 1)] == step([(3, 0), (4, 0)], 'U')
+    assert [(4, 1), (4, 2)] == step([(3, 0), (4, 1)], 'U')
+    assert [(4, 2), (4, 3)] == step([(4, 1), (4, 2)], 'U')
+    assert [(4, 3), (4, 4)] == step([(4, 2), (4, 3)], 'U')
+    assert [(4, 3), (3, 4)] == step([(4, 3), (4, 4)], 'L')
+    assert [(3, 4), (2, 4)] == step([(4, 3), (3, 4)], 'L')
+    assert [(2, 4), (1, 4)] == step([(3, 4), (2, 4)], 'L')
+    assert [(2, 4), (1, 3)] == step([(2, 4), (1, 4)], 'D')
+    assert [(2, 4), (2, 3)] == step([(2, 4), (1, 3)], 'R')
+    assert [(2, 4), (3, 3)] == step([(2, 4), (2, 3)], 'R')
+    assert [(3, 3), (4, 3)] == step([(2, 4), (3, 3)], 'R')
+    assert [(4, 3), (5, 3)] == step([(3, 3), (4, 3)], 'R')
+    assert [(4, 3), (5, 2)] == step([(4, 3), (5, 3)], 'D')
+    assert [(4, 3), (4, 2)] == step([(4, 3), (5, 2)], 'L')
+    assert [(4, 3), (3, 2)] == step([(4, 3), (4, 2)], 'L')
+    assert [(3, 2), (2, 2)] == step([(4, 3), (3, 2)], 'L')
+    assert [(2, 2), (1, 2)] == step([(3, 2), (2, 2)], 'L')
+    assert [(1, 2), (0, 2)] == step([(2, 2), (1, 2)], 'L')
+    assert [(1, 2), (1, 2)] == step([(1, 2), (0, 2)], 'R')
+    assert [(1, 2), (2, 2)] == step([(1, 2), (1, 2)], 'R')
+
+
+def test_all_moves():
+    assert [(1, 2), (2, 2)] == last(all_moves(d09_s01))
+
+
+def test_unique_tail_positions():
+    assert {(0, 0), (1, 0), (2, 0), (3, 0), (4, 1),
+            (4, 2), (4, 3), (3, 4), (2, 4), (3, 3),
+            (3, 2), (2, 2), (1, 2)} == unique_tail_positions(d09_s01)
+
+
+def test_unique_tail_pos_count():
+    assert 13 == unique_tail_pos_count(d09_s01)
+
+
+d09_input = parse(standard_puzzle_input(year=2022, day=9))
+
+
+def test_day08_part1_soln():
+    assert 5874 == part1(d09_input)
 
 
 # def test_day09_part2_soln():
