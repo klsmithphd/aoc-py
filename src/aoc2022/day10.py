0d77adf41c28d9cee703ce35f78955a92e995a6e
Ken Smith <ken@klsmith.me>
2023-09-10 22:35:29 +0000

    2022 day10 soln

--- src/aoc2022/day10.py
+++ src/aoc2022/day10.py
@@ -1,18 +1,86 @@
 """Solution to https://adventofcode.com/2022/day/10"""
-# from utils.core import AoCSolution
+from itertools import accumulate, islice, repeat
+from more_itertools import chunked, flatten
+from operator import add, mul
+from utils.core import AoCSolution
 
 # Input parsing
 
 
+def parse_line(line):
+    if line == "noop":
+        return "noop"
+    else:
+        _, value = line.split()
+        return int(value)
+
+
 def parse(input):
-    return input
+    return [parse_line(line) for line in input]
 
 # Puzzle logic
 
 
+def addends(op):
+    return [0] if op == "noop" else [0, op]
+
+
+def changes(cmds):
+    return flatten(addends(cmd) for cmd in cmds)
+
+
+def register_values(input):
+    return accumulate(changes(input), add, initial=1)
+
+
+def signal_samples(input):
+    """
+    Returns the value of the X register **during** the 20th cycle
+    and every 40 cycles after that
+    """
+    return islice(register_values(input), 19, None, 40)
+
+
+def signal_strength_sum(input):
+    """
+    The signal strength is the cycle number multipled by the value of the
+    X register during that cycle
+    """
+    return sum(map(mul, signal_samples(input), [20, 60, 100, 140, 180, 220]))
+
+
+def light(pixel, register):
+    """
+    Determine whether to light up the screen at the `pixel` location,
+    given the value of the X `register`
+    """
+    return "#" if (abs(register - pixel) <= 1) else " "
+
+
+def screen(input):
+    """
+    """
+    lights = map(light, flatten(repeat(range(40), 6)), register_values(input))
+    return '\n'.join(map(''.join, chunked(lights, 40)))
+
+
 # Puzzle solutions
 
-# day10_soln = \
-#     AoCSolution(parse,
-#                 p1=
-#                 p2=)
+
+def part1(input):
+    """
+    Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 
+    220th cycles. What is the sum of these six signal strengths?
+    """
+    return signal_strength_sum(input)
+
+
+def part2(input):
+    """
+    Render the image given by your program. What eight capital letters 
+    appear on your CRT?
+    """
+    return screen(input)
+
+
+day10_soln = AoCSolution(parse, part1, part2)
--- tests/aoc2022/test_day10.py
+++ tests/aoc2022/test_day10.py
@@ -1,13 +1,202 @@
-# from aoc2022.day10 import day10_soln
+from aoc2022.day10 import parse, part1, part2,\
+    register_values, signal_samples, signal_strength_sum, screen
 from utils.core import standard_puzzle_input
 
+d10_s01_raw = """noop
+addx 3
+addx -5""".splitlines()
 
-d10_input = standard_puzzle_input(year=2022, day=10)
+d10_s02_raw = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""".splitlines()
+
+d10_s01 = ["noop", 3, -5]
+d10_s02 = parse(d10_s02_raw)
+
+d10_s02_screen = """##  ##  ##  ##  ##  ##  ##  ##  ##  ##  
+###   ###   ###   ###   ###   ###   ### 
+####    ####    ####    ####    ####    
+#####     #####     #####     #####     
+######      ######      ######      ####
+#######       #######       #######     """
 
 
-# def test_day10_part1_soln():
-#     assert 1 == day10_soln.part1(d10_input)
+def test_parse():
+    assert d10_s01 == parse(d10_s01_raw)
 
 
-# def test_day10_part2_soln():
-#     assert 1 == day10_soln.part2(d10_input)
+def test_register_values():
+    assert [1, 1, 1, 4, 4, -1] == list(register_values(d10_s01))
+
+
+def test_signal_samples():
+    assert [21, 19, 18, 21, 16, 18] == list(signal_samples(d10_s02))
+
+
+def test_signal_strength_sum():
+    assert 13140 == signal_strength_sum(d10_s02)
+
+
+def test_screen():
+    assert d10_s02_screen == screen(d10_s02)
+
+
+d10_input = parse(standard_puzzle_input(year=2022, day=10))
+# ECZUZALR
+d10_input_screen = """####  ##  #### #  # ####  ##  #    ###  
+#    #  #    # #  #    # #  # #    #  # 
+###  #      #  #  #   #  #  # #    #  # 
+#    #     #   #  #  #   #### #    ###  
+#    #  # #    #  # #    #  # #    # #  
+####  ##  ####  ##  #### #  # #### #  # """
+
+
+def test_day10_part1_soln():
+    assert 16020 == part1(d10_input)
+
+
+def test_day10_part2_soln():
+    assert d10_input_screen == part2(d10_input)
