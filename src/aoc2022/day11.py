60f9e2633b4a9dfa3337f14e8ce7004a0453df05
Ken Smith <ken@klsmith.me>
2023-09-16 18:41:56 +0000

    2022 day11 part1 start

--- src/aoc2022/day11.py
+++ src/aoc2022/day11.py
@@ -1,14 +1,102 @@
 """Solution to https://adventofcode.com/2022/day/11"""
-# from utils.core import AoCSolution
+from collections import defaultdict, namedtuple
+from functools import partial
+from itertools import takewhile
+from toolz import drop, first, iterate
+from utils.core import AoCSolution, split_at_blanklines
 
 # Input parsing
 
+Monkey = namedtuple('Monkey', ['operation', 'divisor', 't_dest', 'f_dest'])
+
+L2_START = len("  Starting items: ")
+L3_START = len("  Operation: new = ")
+L4_START = len("  Test: divisible by ")
+L5_START = len("    If true: throw to monkey ")
+L6_START = len("    If false: throw to monkey ")
+
+
+def parse_operation(opstr):
+    _, operand, arg = opstr.split()
+    return [operand, None if arg == "old" else int(arg)]
+
+
+def parse_monkey(monkey_spec):
+    _, l2, l3, l4, l5, l6 = monkey_spec
+    return (Monkey(
+        parse_operation(l3[L3_START:]),
+        int(l4[L4_START:]),
+        int(l5[L5_START:]),
+        int(l6[L6_START:])
+    ),
+        [int(item) for item in l2[L2_START:].split(", ")])
+
 
 def parse(input):
-    return input
+    monkey_data = [parse_monkey(x) for x in split_at_blanklines(input)]
+    return {
+        'monkeys': [monkey[0] for monkey in monkey_data],
+        'items':   [monkey[1] for monkey in monkey_data]
+    }
 
 # Puzzle logic
 
+
+def dict_of_lists(tuples):
+    result = defaultdict(list)
+    for k, v in tuples:
+        result[k].append(v)
+    return result
+
+
+def operate(operation, item):
+    op, arg = operation
+    if op == '+':
+        return item + arg
+    else:
+        return item * (arg if arg else item)
+
+
+def worry(operation, item):
+    return operate(operation, item) // 3
+
+
+def division_test(worry, divisor):
+    return worry % divisor == 0
+
+
+def next_toss(monkey, item):
+    operation, divisor, t_dest, f_dest = monkey
+    wry = worry(operation, item)
+    divides = division_test(wry, divisor)
+    dest = t_dest if divides else f_dest
+    return (dest, wry)
+
+
+def next_tosses(monkey, items):
+    return dict_of_lists(next_toss(monkey, item) for item in items)
+
+
+# def turn(monkeys, items, monkey_id):
+#     tosses = next_tosses(monkeys[monkey_id], items[monkey_id])
+#     new_items = [items[i] + tosses[i] for i in range(len(items))]
+#     new_items[monkey_id] = []
+#     return new_items
+
+def turn(monkeys, monkey_turn):
+    id, items = monkey_turn
+    try:
+        tosses = next_tosses(monkeys[id], items[id])
+        new_items = [items[i] + tosses[i] for i in range(len(items))]
+        new_items[id] = []
+        return (id+1, new_items)
+    except:
+        raise IndexError
+
+
+def round(monkeys, items):
+    return first(drop(len(monkeys),
+                      iterate(partial(turn, monkeys), (0, items))))[1]
 
 # Puzzle solutions
 
--- tests/aoc2022/test_day11.py
+++ tests/aoc2022/test_day11.py
@@ -1,8 +1,100 @@
-# from aoc2022.day11 import day11_soln
-from utils.core import standard_puzzle_input
+from aoc2022.day11 import parse, Monkey, turn, round
+# from utils.core import standard_puzzle_input
+
+d11_s01_raw = """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1""".splitlines()
+
+d11_s01 = {
+    'monkeys': [
+        Monkey(operation=['*',   19], divisor=23, t_dest=2, f_dest=3),
+        Monkey(operation=['+',    6], divisor=19, t_dest=2, f_dest=0),
+        Monkey(operation=['*', None], divisor=13, t_dest=1, f_dest=3),
+        Monkey(operation=['+',    3], divisor=17, t_dest=0, f_dest=1)
+    ],
+    'items': [
+        [79, 98],
+        [54, 65, 75, 74],
+        [79, 60, 97],
+        [74]
+    ]
+}
 
 
-d11_input = standard_puzzle_input(year=2022, day=11)
+def test_parse():
+    assert d11_s01 == parse(d11_s01_raw)
+
+
+def test_turn():
+    assert (1,
+            [[],
+             [54, 65, 75, 74],
+             [79, 60, 97],
+             [74, 500, 620]]) == turn(d11_s01['monkeys'], (0, d11_s01['items']))
+    assert (2,
+            [[20, 23, 27, 26],
+             [],
+             [79, 60, 97],
+             [74, 500, 620]]) == turn(d11_s01['monkeys'],
+                                      (1,
+                                       [[],
+                                        [54, 65, 75, 74],
+                                           [79, 60, 97],
+                                           [74, 500, 620]]))
+    assert (3,
+            [[20, 23, 27, 26],
+             [2080],
+             [],
+             [74, 500, 620, 1200, 3136]]) == turn(d11_s01['monkeys'],
+                                                  (2,
+                                                   [[20, 23, 27, 26],
+                                                    [],
+                                                       [79, 60, 97],
+                                                       [74, 500, 620]]))
+    assert (4,
+            [[20, 23, 27, 26],
+             [2080, 25, 167, 207, 401, 1046],
+             [],
+             []]) == turn(d11_s01['monkeys'],
+                          (3,
+                           [[20, 23, 27, 26],
+                            [2080],
+                               [],
+                               [74, 500, 620, 1200, 3136]]))
+
+
+def test_round():
+    assert [[20, 23, 27, 26],
+            [2080, 25, 167, 207, 401, 1046],
+            [],
+            []] == round(d11_s01['monkeys'], d11_s01['items'])
+
+
+# d11_input = standard_puzzle_input(year=2022, day=11)
 
 
 # def test_day11_part1_soln():
