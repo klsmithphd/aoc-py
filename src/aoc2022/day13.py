14734003944ccc72041b20b8f15d64b8ff7fa29f
Ken Smith <ken@klsmith.me>
2023-09-23 22:40:56 +0000

    2022 Day 13 part1

--- src/aoc2022/day13.py
+++ src/aoc2022/day13.py
@@ -1,16 +1,59 @@
 """Solution to https://adventofcode.com/2022/day/13"""
-# from utils.core import AoCSolution
+from utils.core import split_at_blanklines
+from toolz import first
 
 # Input parsing
 
 
+def parse_pairs(pairs):
+    return [eval(x) for x in pairs]
+
+
 def parse(input):
-    return input
+    return [parse_pairs(pair) for pair in split_at_blanklines(input)]
 
 # Puzzle logic
 
 
+def compare(a, b):
+    """https://docs.python.org/3.0/whatsnew/3.0.html#ordering-comparisons"""
+    return (a > b) - (a < b)
+
+
+def vector_compare(a, b):
+    order_cmp = [x for x in map(packet_compare, a, b) if x != 0]
+    if len(order_cmp) == 0:
+        return compare(len(a), len(b))
+    else:
+        return order_cmp[0]
+
+
+def packet_compare(a, b):
+    if isinstance(a, int):
+        if isinstance(b, int):
+            return compare(a, b)
+        else:
+            return packet_compare([a], b)
+    else:
+        if isinstance(b, int):
+            return packet_compare(a, [b])
+        else:
+            return vector_compare(a, b)
+
+
+def isinorder(a, b):
+    return packet_compare(a, b) <= 0
+
+
+def inorder_packet_id_sum(input):
+    return sum(idx+1 for idx, pair in enumerate(input) if isinorder(*pair))
+
+
 # Puzzle solutions
+
+def part1(input):
+    return inorder_packet_id_sum(input)
+
 
 # day13_soln = \
 #     AoCSolution(parse,
--- tests/aoc2022/test_day13.py
+++ tests/aoc2022/test_day13.py
@@ -1,12 +1,81 @@
-# from aoc2022.day13 import day13_soln
+from aoc2022.day13 import parse, part1, isinorder, inorder_packet_id_sum
 from utils.core import standard_puzzle_input
 
+d13_s0_raw = """[1,1,3,1,1]
+[1,1,5,1,1]
 
-d13_input = standard_puzzle_input(year=2022, day=13)
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]""".splitlines()
 
 
-# def test_day13_part1_soln():
-#     assert 1 == day13_soln.part1(d13_input)
+d13_s0 = [[[1, 1, 3, 1, 1],
+          [1, 1, 5, 1, 1]],
+
+          [[[1], [2, 3, 4]],
+           [[1], 4]],
+
+          [[9],
+           [[8, 7, 6]]],
+
+          [[[4, 4], 4, 4],
+           [[4, 4], 4, 4, 4]],
+
+          [[7, 7, 7, 7],
+           [7, 7, 7]],
+
+          [[],
+           [3]],
+
+          [[[[]]],
+           [[]]],
+
+          [[1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+           [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]]]
+
+
+def test_parse():
+    assert d13_s0 == parse(d13_s0_raw)
+
+
+def test_isinorder():
+    assert True == isinorder([1, 2], [2])
+    assert True == isinorder(*d13_s0[0])
+    assert True == isinorder(*d13_s0[1])
+    assert False == isinorder(*d13_s0[2])
+    assert True == isinorder(*d13_s0[3])
+    assert False == isinorder(*d13_s0[4])
+    assert True == isinorder(*d13_s0[5])
+    assert False == isinorder(*d13_s0[6])
+    assert False == isinorder(*d13_s0[7])
+
+
+def test_inorder_packet_id_sum():
+    assert 13 == inorder_packet_id_sum(d13_s0)
+
+
+d13_input = parse(standard_puzzle_input(year=2022, day=13))
+
+
+def test_day13_part1_soln():
+    assert 5503 == part1(d13_input)
 
 
 # def test_day13_part2_soln():
