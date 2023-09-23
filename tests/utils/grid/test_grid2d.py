a3e197f0dc5239f967c5c1ce1e08ca9ecbd5578e
Ken Smith <ken@klsmith.me>
2023-09-23 20:24:27 +0000

    Rename tests to be consistent

--- tests/utils/grid/test_grid2d.py
+++ tests/utils/grid/test_grid2d.py
@@ -1,9 +1,5 @@
 from utils.grid.grid2d import adj_coords_2d
 
-sample = {(0, 2): 7, (1, 2): 8, (2, 2): 9,
-          (0, 1): 4, (1, 1): 5, (2, 1): 6,
-          (0, 0): 1, (1, 0): 2, (2, 0): 3}
-
 
 def test_adj_coords_2d():
     assert {(0, 1), (1, 0), (0, -1), (-1, 0)} == set(adj_coords_2d((0, 0)))
