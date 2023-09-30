ce68e711e973d82014287410c99c22125e7650b6
Ken Smith <ken@klsmith.me>
2023-09-30 22:42:03 +0000

    Parse logic for 2022-14

--- src/aoc2022/day14.py
+++ src/aoc2022/day14.py
@@ -1,13 +1,34 @@
 """Solution to https://adventofcode.com/2022/day/14"""
+from more_itertools import flatten, sliding_window
 # from utils.core import AoCSolution
 
 # Input parsing
 
 
+def parse_line(line: str):
+    return [tuple(map(int, coord.split(","))) for coord in line.split(" -> ")]
+
+
 def parse(input):
-    return input
+    return [parse_line(line) for line in input]
 
 # Puzzle logic
+
+
+def points(pos1, pos2):
+    x1, y1 = pos1
+    x2, y2 = pos2
+    dy = 1 if y1 <= y2 else -1
+    dx = 1 if x1 <= x2 else -1
+    return [(x, y) for x in range(x1, x2+dx, dx) for y in range(y1, y2+dy, dy)]
+
+
+def trace_lines(line):
+    return flatten(points(*x) for x in sliding_window(line, 2))
+
+
+def rocks(input):
+    return set(flatten(trace_lines(line) for line in input))
 
 
 # Puzzle solutions
--- tests/aoc2022/test_day14.py
+++ tests/aoc2022/test_day14.py
@@ -1,8 +1,40 @@
-# from aoc2022.day14 import day14_soln
+from aoc2022.day14 import parse, rocks
 from utils.core import standard_puzzle_input
 
+d14_s0_raw = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9""".splitlines()
 
-d14_input = standard_puzzle_input(year=2022, day=14)
+d14_s0 = [[(498, 4), (498, 6), (496, 6)],
+          [(503, 4), (502, 4), (502, 9), (494, 9)]]
+
+
+def test_parse():
+    assert d14_s0 == parse(d14_s0_raw)
+
+
+def test_rocks():
+    assert {(498, 4),
+            (498, 5),
+            (498, 6),
+            (497, 6),
+            (496, 6),
+            (503, 4),
+            (502, 4),
+            (502, 5),
+            (502, 6),
+            (502, 7),
+            (502, 8),
+            (502, 9),
+            (501, 9),
+            (500, 9),
+            (499, 9),
+            (498, 9),
+            (497, 9),
+            (496, 9),
+            (495, 9),
+            (494, 9)} == rocks(d14_s0)
+
+# d14_input = standard_puzzle_input(year=2022, day=14)
 
 
 # def test_day14_part1_soln():
