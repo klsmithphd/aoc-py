0487959abd3f22d2b5a3b07108e65309e4c01fb2
Ken Smith <ken@klsmith.me>
2024-01-06 23:52:41 +0000

    2015 Day06 WIP

---
+++ src/aoc2015/day06.py
@@ -0,0 +1,78 @@
+"""Solution to https://adventofcode.com/2015/day/6"""
+import cardinality
+import collections
+import functools
+import itertools as it
+import re
+
+
+# Input parsing
+Instruction = collections.namedtuple("Instruction", ["cmd", "start", "end"])
+pattern = r"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)"
+
+
+def parse_line(line: str):
+    a, b, c, d, e = re.findall(pattern, line)[0]
+    return Instruction(a, (int(b), int(c)), (int(d), int(e)))
+
+
+def parse(input):
+    return [parse_line(line) for line in input]
+
+
+# Puzzle logic
+def init_grid():
+    return list(it.repeat(list(it.repeat(0, 1000)), 1000))
+
+
+toggle = {0: 1, 1: 0}
+
+
+def part1_update(grid, cmd, x, y):
+    match cmd:
+        case "turn on":  grid[y][x] = 1
+        case "turn off": grid[y][x] = 0
+        case "toggle":   grid[y][x] = toggle[grid[y][x]]
+
+
+def dec_min_zero(x):
+    if x == 0:
+        return x
+    else:
+        return x-1
+
+
+def part2_update(grid, cmd, x, y):
+    match cmd:
+        case "turn on":  grid[y][x] += 1
+        case "turn off": grid[y][x] = dec_min_zero(grid[y][x])
+        case "toggle":   grid[y][x] += 2
+
+
+def update_grid(update_fn, grid, instruction):
+    cmd, start, end = instruction
+    sx, sy = start
+    ex, ey = end
+    for y in range(sy, ey+1):
+        for x in range(sx, ex+1):
+            update_fn(grid, cmd, x, y)
+    return grid
+
+
+def brightness(grid):
+    return sum(it.chain(*grid))
+
+
+# Puzzle solutions
+def part1(input):
+    grid = init_grid()
+    for inst in input:
+        update_grid(part1_update, grid, inst)
+    return brightness(grid)
+
+
+def part2(input):
+    grid = init_grid()
+    for inst in input:
+        update_grid(part2_update, grid, inst)
+    return brightness(grid)
---
+++ tests/aoc2015/test_day06.py
@@ -0,0 +1,25 @@
+import aoc2015.day06 as d06
+import utils.core as u
+
+d06_s00_raw = ["turn on 0,0 through 999,999",
+               "toggle 0,0 through 999,0",
+               "turn off 499,499 through 500,500"]
+
+d06_s00 = [d06.Instruction("turn on",  (0, 0),     (999, 999)),
+           d06.Instruction("toggle",   (0, 0),     (999, 0)),
+           d06.Instruction("turn off", (499, 499), (500, 500))]
+
+
+def test_parse():
+    assert d06_s00 == d06.parse(d06_s00_raw)
+
+
+day06_input = d06.parse(u.standard_puzzle_input(year=2015, day=6))
+
+
+def test_part1():
+    assert 377891 == d06.part1(day06_input)
+
+
+def test_part2():
+    assert 14110788 == d06.part2(day06_input)
