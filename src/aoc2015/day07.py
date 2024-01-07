9d5d7c4601af601fe3d686bd8183127b7e67b1b3
Ken Smith <ken@klsmith.me>
2024-01-07 18:51:06 +0000

    2015 Day 07 soln

--- src/aoc2015/day07.py
+++ src/aoc2015/day07.py
@@ -1,7 +1,10 @@
 """Solution to https://adventofcode.com/2015/day/7"""
+import frozendict
+import functools as ft
 import re
 
 
+# Input parsing
 def tokenize(s: str):
     return int(s) if s.isnumeric() else s
 
@@ -32,4 +35,48 @@ def parse_line(line: str):
 
 
 def parse(input):
-    return dict(parse_line(line) for line in input)
+    return frozendict.frozendict(parse_line(line) for line in input)
+
+
+# Puzzle logic
+@ft.cache
+def wire_val(circuit, wire):
+    """Determines the signal provided to the given wire by the circuit by
+    recursively evaluating its connections in the circuit.
+
+    This approach requires memoization provided by functools' cache decoration"""
+    op, args = circuit[wire]
+    match op:
+        case "assign":
+            return wire_val(circuit, args) if isinstance(args, str) else args
+        case "or":
+            return wire_val(circuit, args[0]) | wire_val(circuit, args[1])
+        case "and":
+            if isinstance(args[0], int):
+                return args[0] & wire_val(circuit, args[1])
+            else:
+                return wire_val(circuit, args[0]) & wire_val(circuit, args[1])
+        case "lshift":
+            return wire_val(circuit, args[0]) << args[1]
+        case "rshift":
+            return wire_val(circuit, args[0]) >> args[1]
+        case "not":
+            # We need to AND with 2^16-1 (65535) in order to trim out the
+            # higher order bits present after we bit-not the value
+            # This wouldn't be necessary if we could guarantee we're working
+            # with 2-byte (16-bit) unsigned integers
+            return 65535 & ~wire_val(circuit, args[0])
+
+
+# Puzzle solutions
+def part1(input):
+    """Determine the signal provided to wire a"""
+    return wire_val(input, "a")
+
+
+def part2(input):
+    """Take the signal from a in part1, feed it to wire b, and recompute 
+    a's signal"""
+    a_val = wire_val(input, "a")
+    new_circuit = input.set("b", ("assign", a_val))
+    return wire_val(new_circuit, "a")
--- tests/aoc2015/test_day07.py
+++ tests/aoc2015/test_day07.py
@@ -1,5 +1,6 @@
 import aoc2015.day07 as d07
-
+import frozendict
+import utils.core as u
 
 d07_s00_raw = [
     "123 -> x",
@@ -14,7 +15,7 @@ d07_s00_raw = [
     "f -> j"
 ]
 
-d07_s00 = {
+d07_s00 = frozendict.frozendict({
     "x": ("assign", 123),
     "y": ("assign", 456),
     "d": ("and",    ("x", "y")),
@@ -24,8 +25,31 @@ d07_s00 = {
     "h": ("not",    ("x",)),
     "i": ("not",    ("y",)),
     "j": ("assign", "f")
-}
+})
 
 
 def test_parse():
     assert d07_s00 == d07.parse(d07_s00_raw)
+
+
+def test_wire_val():
+    assert 72 == d07.wire_val(d07_s00, "d")
+    assert 507 == d07.wire_val(d07_s00, "e")
+    assert 492 == d07.wire_val(d07_s00, "f")
+    assert 114 == d07.wire_val(d07_s00, "g")
+    assert 65412 == d07.wire_val(d07_s00, "h")
+    assert 65079 == d07.wire_val(d07_s00, "i")
+    assert 123 == d07.wire_val(d07_s00, "x")
+    assert 456 == d07.wire_val(d07_s00, "y")
+    assert 492 == d07.wire_val(d07_s00, "j")
+
+
+day07_input = d07.parse(u.standard_puzzle_input(year=2015, day=7))
+
+
+def test_part1():
+    assert 3176 == d07.part1(day07_input)
+
+
+def test_part2():
+    assert 14710 == d07.part2(day07_input)
