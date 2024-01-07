b5cb54040bded1f09ac4b1ed7cf129dfc2d6a5a5
Ken Smith <ken@klsmith.me>
2024-01-07 21:07:12 +0000

    2015 Day08 complete

--- tests/aoc2015/test_day08.py
+++ tests/aoc2015/test_day08.py
@@ -8,17 +8,47 @@ d08_s00 = [
     r'"\x27"'
 ]
 
+d08_s00_unescaped = [
+    "",
+    "abc",
+    "aaa\"aaa",
+    "\x27"
+]
 
-def test_code_chars():
-    assert [2, 5, 10, 6] == [d08.code_chars(x) for x in d08_s00]
+d08_s08_escaped = [
+    r'"\"\""',
+    r'"\"abc\""',
+    r'"\"aaa\\\"aaa\""',
+    r'"\"\\x27\""'
+]
 
 
-def test_string_chars():
-    assert [0, 3, 7, 1] == [d08.string_chars(x) for x in d08_s00]
+def test_unescape():
+    assert d08_s00_unescaped == [d08.unescape(x) for x in d08_s00]
+
+
+def test_escape():
+    assert d08_s08_escaped == [d08.escape(x) for x in d08_s00]
+
+
+def test_code_count():
+    assert [2, 5, 10, 6] == [len(x) for x in d08_s00]
+
+
+def test_unescaped_count():
+    assert [0, 3, 7, 1] == [len(d08.unescape(x)) for x in d08_s00]
+
+
+def test_escaped_count():
+    assert [6, 9, 16, 11] == [len(d08.escape(x)) for x in d08_s00]
 
 
 def test_unescaped_diff():
     assert 12 == d08.unescaped_diff(d08_s00)
+
+
+def test_escaped_diff():
+    assert 19 == d08.escaped_diff(d08_s00)
 
 
 day08_input = d08.parse(u.standard_puzzle_input(year=2015, day=8))
@@ -26,3 +56,7 @@ day08_input = d08.parse(u.standard_puzzle_input(year=2015, day=8))
 
 def test_part1():
     assert 1371 == d08.part1(day08_input)
+
+
+def test_part2():
+    assert 2117 == d08.part2(day08_input)
