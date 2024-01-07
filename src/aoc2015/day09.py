7011254deaf2c8597947b026428ec7da419c458e
Ken Smith <ken@klsmith.me>
2024-01-07 21:55:39 +0000

    2015 Day09 soln

--- src/aoc2015/day09.py
+++ src/aoc2015/day09.py
@@ -12,3 +12,36 @@ def parse_line(line: str):
 def parse(input):
     dists = sorted(it.chain.from_iterable(parse_line(line) for line in input))
     return {k: {b: c for _, b, c in g} for k, g in it.groupby(dists, toolz.first)}
+
+
+# Puzzle logic
+def route_distance(dists, route):
+    """Computes the distance along a route denoted as a sequence of nodes"""
+    return sum(dists[a][b] for a, b in it.pairwise(route))
+
+
+def all_route_distances(dists):
+    """Computes all possible route distances"""
+    nodes = dists.keys()
+    return (route_distance(dists, route) for route in it.permutations(nodes))
+
+
+def shortest_route_distance(dists):
+    """Returns the shortest possible route distance"""
+    return min(all_route_distances(dists))
+
+
+def longest_route_distance(dists):
+    """Returns the longest possible route distance"""
+    return max(all_route_distances(dists))
+
+
+# Puzzle solutions
+def part1(input):
+    """Distance of shortest route visiting every location once"""
+    return shortest_route_distance(input)
+
+
+def part2(input):
+    """Distance of longest route visiting every location once"""
+    return longest_route_distance(input)
--- tests/aoc2015/test_day09.py
+++ tests/aoc2015/test_day09.py
@@ -1,4 +1,5 @@
 import aoc2015.day09 as d09
+import utils.core as u
 
 
 d09_s00_raw = [
@@ -15,3 +16,23 @@ d09_s00 = {
 
 def test_parse():
     assert d09_s00 == d09.parse(d09_s00_raw)
+
+
+def test_route_distance():
+    assert 982 == d09.route_distance(d09_s00, ["Dublin", "London", "Belfast"])
+    assert 605 == d09.route_distance(d09_s00, ["London", "Dublin", "Belfast"])
+    assert 659 == d09.route_distance(d09_s00, ["London", "Belfast", "Dublin"])
+    assert 659 == d09.route_distance(d09_s00, ["Dublin", "Belfast", "London"])
+    assert 605 == d09.route_distance(d09_s00, ["Belfast", "Dublin", "London"])
+    assert 982 == d09.route_distance(d09_s00, ["Belfast", "London", "Dublin"])
+
+
+day09_input = d09.parse(u.standard_puzzle_input(year=2015, day=9))
+
+
+def test_part1():
+    assert 207 == d09.part1(day09_input)
+
+
+def test_part2():
+    assert 804 == d09.part2(day09_input)
