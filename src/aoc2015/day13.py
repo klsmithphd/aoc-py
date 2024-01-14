04d04f59fce8671b5036061f503c35689bee197a
Ken Smith <ken@klsmith.me>
2024-01-14 23:58:29 +0000

    2015 Day13 arrangements

--- src/aoc2015/day13.py
+++ src/aoc2015/day13.py
@@ -1,1 +1,25 @@
 """Solution to https://adventofcode.com/2015/day/13"""
+import itertools as it
+import toolz
+
+# Input parsing
+
+
+def parse_line(line: str):
+    pieces = line.split()
+    person1 = pieces[0]
+    gain_or_loss = 1 if pieces[2] == 'gain' else -1
+    amount = int(pieces[3])
+    person2 = pieces[10][:-1]
+    return (person1, person2, gain_or_loss * amount)
+
+
+def parse(input):
+    groups = it.groupby((parse_line(x) for x in input), lambda x: x[0])
+    return {p1: {p2: h for _, p2, h in v} for p1, v in groups}
+
+
+def arrangements(prefs: dict):
+    people = prefs.keys()
+    p1 = toolz.first(people)
+    return ((p1, *x) for x in it.permutations(it.islice(people, 1, None)))
--- tests/aoc2015/test_day13.py
+++ tests/aoc2015/test_day13.py
@@ -1,6 +1,6 @@
+import aoc2015.day13 as d13
 
-
-d13_s00 = [
+d13_s00_raw = [
     "Alice would gain 54 happiness units by sitting next to Bob.",
     "Alice would lose 79 happiness units by sitting next to Carol.",
     "Alice would lose 2 happiness units by sitting next to David.",
@@ -14,3 +14,12 @@ d13_s00 = [
     "David would lose 7 happiness units by sitting next to Bob.",
     "David would gain 41 happiness units by sitting next to Carol.",
 ]
+
+d13_s00 = {"Alice": {"Bob": 54,    "Carol": -79, "David": -2},
+           "Bob":   {"Alice": 83,  "Carol": -7,  "David": -63},
+           "Carol": {"Alice": -62, "Bob": 60,    "David": 55},
+           "David": {"Alice": 46,  "Bob": -7,    "Carol": 41}}
+
+
+def test_parse():
+    assert d13_s00 == d13.parse(d13_s00_raw)
