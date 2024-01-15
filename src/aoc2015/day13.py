fa5b3c20aa9e56d67d6e866a52346b5606bc891b
Ken Smith <ken@klsmith.me>
2024-01-15 21:18:39 +0000

    2015 Day13 soln

--- src/aoc2015/day13.py
+++ src/aoc2015/day13.py
@@ -1,10 +1,10 @@
 """Solution to https://adventofcode.com/2015/day/13"""
 import itertools as it
+import more_itertools as mit
 import toolz
 
+
 # Input parsing
-
-
 def parse_line(line: str):
     pieces = line.split()
     person1 = pieces[0]
@@ -19,7 +19,60 @@ def parse(input):
     return {p1: {p2: h for _, p2, h in v} for p1, v in groups}
 
 
+# Puzzle logic
 def arrangements(prefs: dict):
+    """Returns the (n-1)! possible arrangements of the guests around the table.
+
+    Technically there should be only half of that number to account because
+    the clockwise/counter-clockwise orderings around the table are equivalent"""
     people = prefs.keys()
     p1 = toolz.first(people)
     return ((p1, *x) for x in it.permutations(it.islice(people, 1, None)))
+
+
+def table_circle(coll: list):
+    """Takes a collection of n elements and makes it into a collection 2 items
+    longer, wrapping around the first and second elements to the end, i.e.,
+    it take [1, 2, 3, 4] and turns it into [1, 2, 3, 4, 1, 2]. This can
+    then be used to take 3 elements at a time and compute the happiness
+    of the center person"""
+    return it.chain(coll, coll[:2])
+
+
+def happiness(prefs, triad):
+    """Computes a happiness score for a given guest based on who their 
+    neighbors are and their known preferences"""
+    n1, guest, n2 = triad
+    return prefs[guest][n1] + prefs[guest][n2]
+
+
+def happiness_total(prefs: dict, order: list):
+    """Compute the total happiness score for the given ordering of guests"""
+    triads = mit.windowed(table_circle(order), 3, 1)
+    return sum(happiness(prefs, triad) for triad in triads)
+
+
+def max_happiness(prefs):
+    """Finds the maximum happiness of any possible configuration of guests"""
+    return max(happiness_total(prefs, order) for order in arrangements(prefs))
+
+
+def add_me(prefs: dict):
+    """Insert a new node (me) into the mix, whose happiness scores all all zero"""
+    def with_me(v: dict):
+        v.update({'Me': 0})
+        return v
+    new_prefs = {k: with_me(v) for k, v in prefs.items()}
+    new_prefs.update({"Me": {g: 0 for g in prefs.keys()}})
+    return new_prefs
+
+
+# Puzzle solutions
+def part1(input):
+    """Find the maximum happiness score possible for all guests."""
+    return max_happiness(input)
+
+
+def part2(input):
+    """Find the maximum happiness score including me in the seating plan"""
+    return max_happiness(add_me(input))
--- tests/aoc2015/test_day13.py
+++ tests/aoc2015/test_day13.py
@@ -1,4 +1,5 @@
 import aoc2015.day13 as d13
+import utils.core as u
 
 d13_s00_raw = [
     "Alice would gain 54 happiness units by sitting next to Bob.",
@@ -15,11 +16,50 @@ d13_s00_raw = [
     "David would gain 41 happiness units by sitting next to Carol.",
 ]
 
-d13_s00 = {"Alice": {"Bob": 54,    "Carol": -79, "David": -2},
-           "Bob":   {"Alice": 83,  "Carol": -7,  "David": -63},
-           "Carol": {"Alice": -62, "Bob": 60,    "David": 55},
-           "David": {"Alice": 46,  "Bob": -7,    "Carol": 41}}
+d13_s00 = {
+    "Alice": {"Bob": 54,    "Carol": -79, "David": -2},
+    "Bob":   {"Alice": 83,  "Carol": -7,  "David": -63},
+    "Carol": {"Alice": -62, "Bob": 60,    "David": 55},
+    "David": {"Alice": 46,  "Bob": -7,    "Carol": 41}
+}
+
+d13_s00_with_me = {
+    "Alice": {"Bob": 54,    "Carol": -79, "David": -2,  "Me": 0},
+    "Bob":   {"Alice": 83,  "Carol": -7,  "David": -63, "Me": 0},
+    "Carol": {"Alice": -62, "Bob": 60,    "David": 55,  "Me": 0},
+    "David": {"Alice": 46,  "Bob": -7,    "Carol": 41,  "Me": 0},
+    "Me":    {"Alice": 0,   "Bob": 0,     "Carol": 0,  "David": 0}
+}
 
 
 def test_parse():
     assert d13_s00 == d13.parse(d13_s00_raw)
+
+
+def test_arrangements():
+    assert [("Alice", "Bob", "Carol", "David"),
+            ("Alice", "Bob", "David", "Carol"),
+            ("Alice", "Carol", "Bob", "David"),
+            ("Alice", "Carol", "David", "Bob"),
+            ("Alice", "David", "Bob", "Carol"),
+            ("Alice", "David", "Carol", "Bob")] == \
+        list(d13.arrangements(d13_s00))
+
+
+def test_max_happiness():
+    assert 330 == d13.max_happiness(d13_s00)
+
+
+def test_add_me():
+    assert d13_s00_with_me == d13.add_me(d13_s00)
+
+
+day13_input = d13.parse(u.standard_puzzle_input(year=2015, day=13))
+
+
+def test_part1():
+    assert 709 == d13.part1(day13_input)
+
+
+def test_part2():
+    assert 668 == d13.part2(day13_input)
