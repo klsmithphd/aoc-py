8cd76e36c29f59a0eaeeb5906c3f9151c337e716
Ken Smith <ken@klsmith.me>
2024-02-04 13:15:43 +0000

    2015 Day15 part1 soln

--- src/aoc2015/day15.py
+++ src/aoc2015/day15.py
@@ -1,4 +1,7 @@
 """Solution to https://adventofcode.com/2015/day/14"""
+import itertools as it
+import functools as ft
+import operator as op
 import re
 
 # Constants
@@ -16,6 +19,40 @@ def parse(input):
 
 
 # Puzzle logic
+def score_vec(ingredients, quantities):
+    """Computes the score component vector, where each element is the 
+    sum of the products of each ingredients properties and the quantity
+    of that ingredient"""
+    def scalar_mult(ing, qty): return [i*qty for i in ing]
+    amounts = map(scalar_mult, ingredients, quantities)
+    return [x if x >= 0 else 0 for x in map(sum, zip(*amounts))]
+
+
+def score(score_vec):
+    """The cookie score is the product of the first four properties"""
+    return ft.reduce(op.mul, score_vec[:-1])
+
+
+def all_options(total: int, n: int):
+    """Returns a sequence of all possible combinations of `n` items
+    that sum to `total`"""
+    if n == 2:
+        return [[(total - x), x] for x in range(total+1)]
+    else:
+        return [[(total - x)] + sub_option
+                for x in range(total+1)
+                for sub_option in all_options(x, n-1)]
+
+
+def max_score(ingredients):
+    """Computes the maximum possible score for the given ingredients."""
+    options = all_options(MAX_TEASPOONS, len(ingredients))
+    score_vecs = (score_vec(ingredients, opt) for opt in options)
+    return max(score(x) for x in score_vecs)
+    # return max(score(score_vec(ingredients, opt)) for opt in all_options(MAX_TEASPOONS, len(ingredients)))
 
 
 # Puzzle solutions
+def part1(input):
+    """Computes the maximum cookie score for the given ingredients"""
+    return max_score(input)
--- tests/aoc2015/test_day15.py
+++ tests/aoc2015/test_day15.py
@@ -13,3 +13,45 @@ d15_s00 = [[-1, -2, 6, 3, 8],
 def test_parse():
     """Correctly parses the input"""
     assert d15_s00 == d15.parse(d15_s00_raw)
+
+
+def test_score_vec():
+    """Computes the score component vector"""
+    assert [68, 80, 152, 76, 520] == d15.score_vec(d15_s00, [44, 56])
+    assert [80, 100, 120, 60, 500] == d15.score_vec(d15_s00, [40, 60])
+
+
+def test_score():
+    """Computes the cookie score"""
+    # The score component vector for the 44/56 split
+    assert 62842880 == d15.score([68, 80, 152, 76, 520])
+    # The score component vector for the 40/60 split
+    assert 57600000 == d15.score([80, 100, 120, 60, 500])
+
+
+def test_all_options():
+    """Can generate all combinations of n vars that sum to a total"""
+    assert d15.all_options(2, 2) == [[2, 0], [1, 1], [0, 2]]
+    assert d15.all_options(5, 2) == \
+        [[5, 0], [4, 1], [3, 2], [2, 3], [1, 4], [0, 5]]
+    assert d15.all_options(2, 3) == \
+        [[2, 0, 0],
+         [1, 1, 0], [1, 0, 1],
+         [0, 2, 0], [0, 1, 1], [0, 0, 2]]
+    assert d15.all_options(2, 4) == \
+        [[2, 0, 0, 0],
+         [1, 1, 0, 0], [1, 0, 1, 0], [1, 0, 0, 1],
+         [0, 2, 0, 0], [0, 1, 1, 0], [0, 1, 0, 1],
+         [0, 0, 2, 0], [0, 0, 1, 1], [0, 0, 0, 2]]
+
+
+def test_max_score():
+    """Finds the maximum score across all possible combinations"""
+    assert 62842880 == d15.max_score(d15_s00)
+
+
+day15_input = d15.parse(u.standard_puzzle_input(year=2015, day=15))
+
+
+def test_part1():
+    assert 21367368 == d15.max_score(day15_input)
