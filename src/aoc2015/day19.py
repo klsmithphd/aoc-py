4217b7cadd8bb9db721e671c642f9b54ac22c6b3
Ken Smith <ken@klsmith.me>
2024-02-07 23:23:14 +0000

    2015 Part1 soln

--- src/aoc2015/day19.py
+++ src/aoc2015/day19.py
@@ -1,4 +1,6 @@
 """Solution to https://adventofcode.com/2015/day/18"""
+import itertools as it
+import more_itertools as mit
 import utils.core as u
 
 
@@ -10,3 +12,25 @@ def parse_replacement(line: str):
 def parse(input):
     replacements, molecule = u.split_at_blanklines(input)
     return [parse_replacement(r) for r in replacements], molecule[0]
+
+
+# Puzzle logic
+def match_ranges(s: str, pattern: str):
+    size = len(pattern)
+    char_ranges = enumerate(mit.sliding_window(s, size))
+    return ((idx, idx+size) for idx, chars in char_ranges
+            if "".join(chars) == pattern)
+
+
+def single_replacements(s: str, repl):
+    old, new_s = repl
+    return (s[:start]+new_s+s[end:] for start, end in match_ranges(s, old))
+
+
+def distinct_molecules(replacements, molecule):
+    return set(it.chain(*(single_replacements(molecule, r) for r in replacements)))
+
+
+# Puzzle solutions
+def part1(input):
+    return len(distinct_molecules(*input))
--- tests/aoc2015/test_day19.py
+++ tests/aoc2015/test_day19.py
@@ -1,4 +1,5 @@
 import aoc2015.day19 as d19
+import utils.core as u
 
 d19_s00_raw = [
     "e => H",
@@ -41,3 +42,25 @@ d19_s01 = (
 
 def test_parse():
     assert d19_s00 == d19.parse(d19_s00_raw)
+
+
+def test_match_ranges():
+    assert [(0, 2), (2, 4), (4, 6)] == list(d19.match_ranges("HOHOHO", "HO"))
+    assert [(1, 3), (3, 5)] == list(d19.match_ranges("HOHOHO", "OH"))
+    assert [] == list(d19.match_ranges("HOHOHO", "e"))
+    assert [(1, 4), (3, 6)] == list(d19.match_ranges("HOHOHO", "OHO"))
+
+
+def test_single_replacements():
+    assert ["HOOH", "HOHO"] == \
+        list(d19.single_replacements("HOH", ["H", "HO"]))
+    assert ["OHOH", "HOOH"] == \
+        list(d19.single_replacements("HOH", ["H", "OH"]))
+    assert ["HHHH"] == list(d19.single_replacements("HOH", ["O", "HH"]))
+
+
+day19_input = d19.parse(u.standard_puzzle_input(year=2015, day=19))
+
+
+def test_part1():
+    assert 518 == d19.part1(day19_input)
