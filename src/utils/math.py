52dc3a07afdbf4a0220aae42bdc0c799db59a8a9
Ken Smith <ken@klsmith.me>
2024-03-02 22:27:19 +0000

    mod_pow simplification

--- src/utils/math.py
+++ src/utils/math.py
@@ -12,21 +12,21 @@ def mod_square(m, a):
     return (a * a) % m
 
 
-def _mod_sq_and_mul(m, a, last, bit: str):
-    """Applies the square-and-multiply method to perform fast 
-    exponentiation of an integer `a` to the power `n` modulo `m`.
-
-    If the next bit is a zero, return the square of `last` (modulo `m`)
-    If the next bit is a one, return the square of `last` times `a` (mod `m`)"""
-    if bit == '0':
-        return mod_square(m, last)
-    else:
-        return mod_mul(m, a, mod_square(m, last))
-
-
 def mod_pow(m, a, n):
     """Computes a^n mod m"""
     if n == 0:
         return 1
     else:
-        return ft.reduce(ft.partial(_mod_sq_and_mul, m, a), format(n, 'b'), 1)
+        # Applies the square-and-multiply method to perform fast
+        # exponentiation of an integer `a` to the power `n` modulo `m`.
+
+        # If the next bit is a zero, return the square of `last` (modulo `m`)
+        # If the next bit is a one, return the square of `last` # times `a`
+        # (modulo `m`)
+        def sq_and_mul(last, bit):
+            if bit == '0':
+                return mod_square(m, last)
+            else:
+                return mod_mul(m, a, mod_square(m, last))
+
+        return ft.reduce(sq_and_mul, format(n, 'b'), 1)
