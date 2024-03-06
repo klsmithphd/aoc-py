3a12f31dfc975ea714816d287bad1a38a1071ad7
Ken Smith <ken@klsmith.me>
2024-03-06 16:20:21 +0000

    Add interpolated fn to grid module

--- tests/utils/test_grid.py
+++ tests/utils/test_grid.py
@@ -9,3 +9,11 @@ def test_turn():
 def test_forward():
     assert {'pos': (5, 1), 'heading': 'e'} == \
         grid.forward({'pos': (-3, 1), 'heading': 'e'}, 8)
+
+
+def test_interpolated():
+    assert [(1, 4), (2, 4), (3, 4), (4, 4)] == \
+        grid.interpolated((1, 4), (4, 4))
+    assert [(3, 2), (3, 1), (3, 0), (3, -1)] == \
+        grid.interpolated((3, 2), (3, -1))
+    assert [(1, 1)] == grid.interpolated((1, 1), (1, 1))
