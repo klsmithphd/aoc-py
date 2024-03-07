165c63aa1539568507c64f4c860255d9df45c935
Ken Smith <ken@klsmith.me>
2024-03-07 23:29:33 +0000

    Part 1 soln

--- src/aoc2016/day03.py
+++ src/aoc2016/day03.py
@@ -1,4 +1,5 @@
 """Solution to https://adventofcode.com/2016/day/3"""
+import cardinality
 
 
 # Input parsing
@@ -8,3 +9,18 @@ def parse_line(line: str):
 
 def parse(input):
     return [parse_line(line) for line in input]
+
+
+# Puzzle logic
+def isvalidtriangle(triangle):
+    a, b, c = triangle
+    return a + b > c and b + c > a and c + a > b
+
+
+def count_valid(triangles):
+    return cardinality.count(x for x in triangles if isvalidtriangle(x))
+
+
+# Puzzle solutions
+def part1(input):
+    return count_valid(input)
--- tests/aoc2016/test_day03.py
+++ tests/aoc2016/test_day03.py
@@ -1,4 +1,5 @@
 import aoc2016.day03 as d03
+import utils.core as u
 
 d03_s00_raw = [
     "101 301 501",
@@ -21,3 +22,10 @@ d03_s00 = [
 
 def test_parse():
     assert d03_s00 == d03.parse(d03_s00_raw)
+
+
+day03_input = d03.parse(u.standard_puzzle_input(year=2016, day=3))
+
+
+def test_part1():
+    assert 869 == d03.part1(day03_input)
