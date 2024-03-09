090c01e3dabe74071662262f8523fdf06ae652af
Ken Smith <ken@klsmith.me>
2024-03-09 23:10:44 +0000

    Part2 soln and docstrings

--- src/aoc2016/day03.py
+++ src/aoc2016/day03.py
@@ -1,5 +1,7 @@
 """Solution to https://adventofcode.com/2016/day/3"""
 import cardinality
+import itertools as it
+import more_itertools as mit
 
 
 # Input parsing
@@ -13,14 +15,29 @@ def parse(input):
 
 # Puzzle logic
 def isvalidtriangle(triangle):
+    """A triangle is valid if the sum of two sides are strictly greater than
+    the remaining side"""
     a, b, c = triangle
     return a + b > c and b + c > a and c + a > b
 
 
 def count_valid(triangles):
+    """Count of triangles considered valid"""
     return cardinality.count(x for x in triangles if isvalidtriangle(x))
+
+
+def groupby_columns(triangles):
+    """Re-interpret the data with triangles spanning three consecutive rows
+    in the same column"""
+    return it.chain(*(zip(*a) for a in mit.batched(triangles, 3)))
 
 
 # Puzzle solutions
 def part1(input):
+    """How many triangles are valid"""
     return count_valid(input)
+
+
+def part2(input):
+    """How many triangles are valid re-interpreting the data as spanning 3 rows"""
+    return count_valid(groupby_columns(input))
--- tests/aoc2016/test_day03.py
+++ tests/aoc2016/test_day03.py
@@ -24,8 +24,21 @@ def test_parse():
     assert d03_s00 == d03.parse(d03_s00_raw)
 
 
+def test_groupby_columns():
+    assert set([(101, 102, 103),
+                (201, 202, 203),
+                (301, 302, 303),
+                (401, 402, 403),
+                (501, 502, 503),
+                (601, 602, 603)]) == set(d03.groupby_columns(d03_s00))
+
+
 day03_input = d03.parse(u.standard_puzzle_input(year=2016, day=3))
 
 
 def test_part1():
     assert 869 == d03.part1(day03_input)
+
+
+def test_part2():
+    assert 1544 == d03.part2(day03_input)
