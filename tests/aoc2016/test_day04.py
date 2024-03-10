964612f5fa2b13b4892263883e1a28daa947cf4f
Ken Smith <ken@klsmith.me>
2024-03-10 23:06:17 +0000

    decipher test

--- tests/aoc2016/test_day04.py
+++ tests/aoc2016/test_day04.py
@@ -9,11 +9,13 @@ d04_s00_raw = [
 ]
 
 d04_s00 = [
-    (["aaaaa", "bbb", "z", "y", "x"], 123, "abxyz"),
-    (["a", "b", "c", "d", "e", "f", "g", "h"], 987, "abcde"),
-    (["not", "a", "real", "room"], 404, "oarel"),
-    (["totally", "real", "room"], 200, "decoy")
+    d04.Room(["aaaaa", "bbb", "z", "y", "x"], 123, "abxyz"),
+    d04.Room(["a", "b", "c", "d", "e", "f", "g", "h"], 987, "abcde"),
+    d04.Room(["not", "a", "real", "room"], 404, "oarel"),
+    d04.Room(["totally", "real", "room"], 200, "decoy")
 ]
+
+d04_s01 = d04.Room(["qzmt", "zixmtkozy", "ivhz"], 343, "")
 
 
 def test_parse():
@@ -26,6 +28,10 @@ def test_isrealroom():
 
 def test_real_room_sector_id_sum():
     assert 1514 == d04.real_room_sector_id_sum(d04_s00)
+
+
+def test_decipher():
+    assert "very encrypted name" == d04.decipher(d04_s01)
 
 
 day04_input = d04.parse(u.standard_puzzle_input(year=2016, day=4))
