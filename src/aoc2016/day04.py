d807a991ec46cb347c17dfa381bbc4ac908f2d9e
Ken Smith <ken@klsmith.me>
2024-03-11 18:51:39 +0000

    Add docstrings

--- src/aoc2016/day04.py
+++ src/aoc2016/day04.py
@@ -3,10 +3,12 @@ import more_itertools as mit
 import re
 import utils.core as u
 from collections import Counter, namedtuple
+from heapq import nsmallest
 
 
 # Constants
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+TARGET_NAME = "northpole object storage"
 
 
 # Input parsing
@@ -24,23 +26,29 @@ def parse(input):
 
 
 # Puzzle logic
-def sort_key(freq):
-    k, v = freq
-    return (-v, k)
+def freq_alph_sort(frequency):
+    """A key function for sorting the character frequencies first by 
+    frequency (descending) then by character in alphabetic order (ascending)"""
+    ch, freq = frequency
+    return (-freq, ch)
 
 
 def isrealroom(room: Room):
+    """"A room is real if the checksum is the five most common characters
+    in the encrypted name, in desc order, with ties broken by alphabetization"""
     encrypted_name, _, checksum = room
     freqs = Counter("".join(encrypted_name))
-    chars = (c for c, _ in mit.take(5, sorted(freqs.items(), key=sort_key)))
+    chars = (c for c, _ in nsmallest(5, freqs.items(), key=freq_alph_sort))
     return "".join(chars) == checksum
 
 
 def real_room_sector_id_sum(rooms):
+    """The sum of the sector ids of all the real rooms"""
     return sum(room.sector_id for room in rooms if isrealroom(room))
 
 
 def decipher(room: Room):
+    """For a real room, apply a shift cipher to decipher the real room name"""
     encrypted_name, sector_id, _ = room
     mapping = dict(zip(ALPHABET, u.rotate(sector_id % 26, ALPHABET)))
     def decode(s): return "".join(mapping[ch] for ch in s)
@@ -48,16 +56,19 @@ def decipher(room: Room):
 
 
 def north_pole_objects_room(rooms):
-    room = mit.first(r for r in rooms if
-                     isrealroom(r) and
-                     decipher(r) == "northpole object storage")
+    """Find the real room sector id whose decrypted name is 
+    `northpole object storage`"""
+    room = mit.first(r for r in rooms
+                     if isrealroom(r) and decipher(r) == TARGET_NAME)
     return room.sector_id
 
 
 # Puzzle solutions
 def part1(input):
+    """Sum of the sector ids of all the real rooms"""
     return real_room_sector_id_sum(input)
 
 
 def part2(input):
+    """Sector id of the room where North Pole objects are stored"""
     return north_pole_objects_room(input)
