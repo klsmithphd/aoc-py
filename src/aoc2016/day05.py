f5cd9bd5c002a68a9d8080b3f4005f049c886cc2
Ken Smith <ken@klsmith.me>
2024-03-26 19:57:52 +0000

    Part1 soln

--- src/aoc2016/day05.py
+++ src/aoc2016/day05.py
@@ -3,8 +3,25 @@ import hashlib
 import itertools as it
 import more_itertools as mit
 
+# Constants
+CACHED_INDICES = {
+    "abc":      [3231929, 5017308, 5278568, 5357525, 5708769,
+                 6082117, 8036669, 8605828, 8609554, 8760605,
+                 9495334, 10767910, 11039607, 12763908, 13666005,
+                 13753421, 14810753, 15274776, 15819744, 18455182,
+                 20014135, 23595234, 25025007, 25162359, 26439072,
+                 30594017, 31051866, 33050215, 33841441, 34415073],
+    "reyedfim": [797564, 938629, 1617991, 2104453, 2564359,
+                 2834991, 3605750, 7183955, 7292419, 7668370,
+                 8059094, 9738948, 10098451, 10105659, 11395933,
+                 12187005, 13432325, 17274562, 18101341, 19897122,
+                 21475898, 21671457, 21679503, 21842490, 23036372,
+                 23090544, 25067104, 26815976, 27230372, 27410373]
+}
 
 # Input parsing
+
+
 def parse(input):
     return mit.first(input)
 
@@ -22,11 +39,20 @@ def five_zero_indices(prefix: str):
     return (x for x in it.count(0) if isfivezerostart(md5_bytes(f"{prefix}{x}")))
 
 
+def indices_to_try(prefix):
+    return CACHED_INDICES.get(prefix, it.count(0))
+
+
 def five_zero_hashes(prefix: str):
-    indices = it.count(0)
+    indices = indices_to_try(prefix)
     bytes = (md5_bytes(f"{prefix}{idx}") for idx in indices)
     return (hash for hash in bytes if isfivezerostart(hash))
 
 
 def password_part1(prefix: str):
     return "".join(hex(b[2])[2:] for b in mit.take(8, five_zero_hashes(prefix)))
+
+
+# Puzzle solutions
+def part1(input):
+    return password_part1(input)
--- tests/aoc2016/test_day05.py
+++ tests/aoc2016/test_day05.py
@@ -12,3 +12,10 @@ def test_five_zero_indices():
 
 def test_password_part1():
     assert "18f47a30" == d05.password_part1(d05_s00)
+
+
+day05_input = d05.parse(u.standard_puzzle_input(year=2016, day=5))
+
+
+def test_part1():
+    assert "f97c354d" == d05.part1(day05_input)
