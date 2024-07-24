0a02f2b9d5f8d12cc8e801566595354b573722ff
Ken Smith <ken@klsmith.me>
2024-07-24 15:54:13 -0600

    limited impl so far

--- src/aoc2016/day14.py
+++ src/aoc2016/day14.py
@@ -1,2 +1,14 @@
 """Solution to https://adventofcode.com/2016/day/14"""
+import re
+import more_itertools as mit
 import utils.digest as dig
+
+
+# Input parsing
+parse = mit.first
+
+
+# Puzzle logic
+def hastriplechars(s: str):
+    return re.match(r"(.)\1{2}", s)
+
