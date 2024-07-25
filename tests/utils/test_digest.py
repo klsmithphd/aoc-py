46241e4e3124111ee437e9c47987a55da593150f
Ken Smith <ken@klsmith.me>
2024-07-25 17:58:35 +0000

    2016 Day14 part1 soln

--- tests/utils/test_digest.py
+++ tests/utils/test_digest.py
@@ -5,6 +5,9 @@ def test_md5_digest():
     assert dig.md5_digest('abcdef') == \
         b'\xe8\x0b\x50\x17\x09\x89\x50\xfc\x58\xaa\xd8\x3c\x8c\x14\x97\x8e'
 
+def test_md5_str():
+    assert dig.md5_str('abcdef') == 'e80b5017098950fc58aad83c8c14978e'
+
 
 def test_isfivezerostart():
     assert True == dig.isfivezerostart(b'\x00\x00\x01\xff')  # 000001ff
